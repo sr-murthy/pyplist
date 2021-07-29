756fc8a0e45cc35a69b1e671285cfb93947266a6
Sandeep Murthy <sr-murthy@users.noreply.github.com>
2021-07-29 22:38:33 +0100

    Add more test cases for `core.find.find_plists`

--- tests/core/test_find.py
+++ tests/core/test_find.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 
 from pathlib import Path
 from tempfile import (
@@ -8,6 +9,7 @@ from tempfile import (
 from unittest import TestCase
 
 from pyplist.core.find import find_plists
+from pyplist.core.plist import Plist
 
 
 class TestFindPlists(TestCase):
@@ -46,3 +48,154 @@ class TestFindPlists(TestCase):
 
                 self.assertEqual(list(find_plists(target_dir, recursive=False)), [])
 
+    def test__target_dir_has_plist_files_in_root_only__non_recursive_search__correct_plists_generated(self):
+        plist1_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>a</key>
+                        <string>one</string>
+                    </dict>
+                    </plist>
+                    """
+
+        plist2_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>b</key>
+                        <integer>2</integer>
+                    </dict>
+                    </plist>
+                    """
+
+        with TemporaryDirectory() as target_dir:
+            with open(Path(target_dir).joinpath('plist1.plist'), 'wb') as plist1_file:
+                with open(Path(target_dir).joinpath('plist2.plist'), 'wb') as plist2_file:
+                    plist1_file.write(textwrap.dedent(plist1_xml).encode('utf8'))
+                    plist1_file.flush()
+                    expected_plist1 = Plist(plist1_file.name)
+
+                    plist2_file.write(textwrap.dedent(plist2_xml).encode('utf8'))
+                    plist2_file.flush()
+                    expected_plist2 = Plist(plist2_file.name)
+
+                    received_plist1, received_plist2 = list(find_plists(target_dir, recursive=False))
+
+                    assert received_plist1 == expected_plist1
+                    assert received_plist2 == expected_plist2
+
+    def test__target_dir_has_plist_files_in_root_and_subfolder__non_recursive_search__correct_plists_generated(self):
+        plist1_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>a</key>
+                        <string>one</string>
+                    </dict>
+                    </plist>
+                    """
+
+        plist2_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>b</key>
+                        <integer>2</integer>
+                    </dict>
+                    </plist>
+                    """
+
+        with TemporaryDirectory() as target_dir:
+            with open(Path(target_dir).joinpath('plist1.plist'), 'wb') as plist1_file:
+                plist1_file.write(textwrap.dedent(plist1_xml).encode('utf8'))
+                plist1_file.flush()
+                expected_plist1 = Plist(plist1_file.name)
+
+                subfolder = Path(target_dir).joinpath('sub')
+                os.mkdir(subfolder)
+                with open(subfolder.joinpath('plist2.plist'), 'wb') as plist2_file:
+                    plist2_file.write(textwrap.dedent(plist2_xml).encode('utf8'))
+                    plist2_file.flush()
+
+                    received_plist1 = next(find_plists(target_dir, recursive=False))
+
+                    assert received_plist1 == expected_plist1
+
+    def test__target_dir_has_plist_files_in_root_only__recursive_search__correct_plists_generated(self):
+        plist1_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>a</key>
+                        <string>one</string>
+                    </dict>
+                    </plist>
+                    """
+
+        plist2_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>b</key>
+                        <integer>2</integer>
+                    </dict>
+                    </plist>
+                    """
+
+        with TemporaryDirectory() as target_dir:
+            with open(Path(target_dir).joinpath('plist1.plist'), 'wb') as plist1_file:
+                with open(Path(target_dir).joinpath('plist2.plist'), 'wb') as plist2_file:
+                    plist1_file.write(textwrap.dedent(plist1_xml).encode('utf8'))
+                    plist1_file.flush()
+                    expected_plist1 = Plist(plist1_file.name)
+
+                    plist2_file.write(textwrap.dedent(plist2_xml).encode('utf8'))
+                    plist2_file.flush()
+                    expected_plist2 = Plist(plist2_file.name)
+
+                    received_plist1, received_plist2 = list(find_plists(target_dir, recursive=True))
+
+                    assert received_plist1 == expected_plist1
+                    assert received_plist2 == expected_plist2
+
+    def test__target_dir_has_plist_files_in_root_and_subfolder__recursive_search__correct_plists_generated(self):
+        plist1_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>a</key>
+                        <string>one</string>
+                    </dict>
+                    </plist>
+                    """
+
+        plist2_xml = """<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                    <plist version="1.0">
+                    <dict>
+                        <key>b</key>
+                        <integer>2</integer>
+                    </dict>
+                    </plist>
+                    """
+
+        with TemporaryDirectory() as target_dir:
+            with open(Path(target_dir).joinpath('plist1.plist'), 'wb') as plist1_file:
+                plist1_file.write(textwrap.dedent(plist1_xml).encode('utf8'))
+                plist1_file.flush()
+                expected_plist1 = Plist(plist1_file.name)
+
+                subfolder = Path(target_dir).joinpath('sub')
+                os.mkdir(subfolder)
+                with open(subfolder.joinpath('plist2.plist'), 'wb') as plist2_file:
+                    plist2_file.write(textwrap.dedent(plist2_xml).encode('utf8'))
+                    plist2_file.flush()
+                    expected_plist2 = Plist(plist2_file.name)
+
+                    plists = find_plists(target_dir, recursive=True)
+                    received_plist1 = next(plists)
+                    received_plist2 = next(plists)
+
+                    assert received_plist1 == expected_plist1
+                    assert received_plist2 == expected_plist2
